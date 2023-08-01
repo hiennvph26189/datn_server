99432b26aa26610a681ce88d981e22715472de8c
saonguyen <jorrynguyen@gmail.com>
2023-08-01 20:11:14 +0700

    upload

--- src/route/web.js
+++ src/route/web.js
@@ -36,20 +36,9 @@ let initWebRouter = (app)=>{
     router.get('/',(req, res)=>{
         return res.send('adafd')
     }) 
-    router.get('/admin/userController',userController.handleGetUser)
-//     router.get('/get-all-sp',testController.getListCRUD)
-//     router.post('/add-product',upload.single('profile_pic'),testController.getAddProduct)
-//     router.get('/item-products',testController.getOneProduct)
-//     router.post('/edit-product',upload.single('profile_pic'),testController.getEditProduct)
-//     router.post('/delete-products',testController.getDeleteProduct)
+    router.get('/admin/userController',userController.handleGetUser);
+    router.post('/admin/userController',userController.handleGetUser);
     
-//     router.get('/upload', homcontroller.getUploadFilePage);
-// router.post('/upload-profile-pic', upload.single('profile_pic'), homcontroller.handleUploadFile)
-
-   
-
-
-
 
     return app.use("/",router)
 }
