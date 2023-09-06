42f69675fb13ea3d45cb2cba37a05d4d53b97413
Nguyendz102 <thenguyen9603@gmail.com>
2023-09-06 16:10:36 +0700

    nguyen

--- src/services/userService.js
+++ src/services/userService.js
@@ -4,42 +4,42 @@ import sequelize from "../config/queryDatabse"
 import bcrypt from 'bcryptjs';
 
 
- const salt = bcrypt.genSaltSync(10);
- const hashUserPassword = (userPassword) => {
-        let hashPassword = bcrypt.hashSync(userPassword, salt);
-        return hashPassword;
- }
+const salt = bcrypt.genSaltSync(10);
+const hashUserPassword = (userPassword) => {
+    let hashPassword = bcrypt.hashSync(userPassword, salt);
+    return hashPassword;
+}
 
- 
-let handleGetUser = (data)=>{
-    return new Promise(async(resolve, reject)=>{
-       
+
+let handleGetUser = (data) => {
+    return new Promise(async (resolve, reject) => {
+
         try {
             const result = await sequelize.query(`
                 SELECT *
                 FROM users
                 ORDER BY id DESC
                 `, { type: QueryTypes.SELECT });
-          
-                resolve({ 
-                    errCode:0,
-                    errMessage: 'thành công',
-                    result:result
-                 })    
-                 
+
+            resolve({
+                errCode: 0,
+                errMessage: 'thành công',
+                result: result
+            })
+
         } catch (error) {
-             reject(error);
+            reject(error);
         }
-             
-     }) 
+
+    })
 }
-let handleCreateUser = (data)=>{
-    return new Promise(async(resolve, reject)=>{
+let handleCreateUser = (data) => {
+    return new Promise(async (resolve, reject) => {
         try {
             let firstName = data.firstname;
             let lastName = data.lastname;
             let email = data.email;
-            let hashPass = hashUserPassword(data.password);  
+            let hashPass = hashUserPassword(data.password);
             let phoneNumber = data.phonenumber;
             let address = data.address;
             let roleID = data.gioitinh;
@@ -48,110 +48,107 @@ let handleCreateUser = (data)=>{
             INSERT INTO users (firstName, lastName, email, password, phoneNumber, address, roleID, createdAT, updatedAT)
             VALUES ('${firstName}', '${lastName}', '${email}', '${hashPass}', '${phoneNumber}', '${address}', '${roleID}', '${today}', '${today}');
                 `, { type: QueryTypes.INSERT });
-          
-                resolve({ 
-                    errCode:0,
-                    errMessage: 'thành công',
-                    result:result
-                 })     
-  
+
+            resolve({
+                errCode: 0,
+                errMessage: 'thành công',
+                result: result
+            })
+
         } catch (error) {
-             reject(error);
+            reject(error);
         }
-        
-     }) 
+
+    })
 }
-    let deleteUser = (id) => {
-        return new Promise(async(resolve, reject)=>{
-            try { 
-                const checkuser = await sequelize.query(`
+let deleteUser = (id) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const checkuser = await sequelize.query(`
             SELECT *
             FROM users
              WHERE id='${id}';
                 `, { type: QueryTypes.SELECT });
-                 if(checkuser.length<=0){
-                        resolve({ 
-                            errCode:1,
-                            errMessage: 'User Khong ton tai',
-                            result:null
-                         })     
-                    }else{
-                        const result = await sequelize.query(`
+            if (checkuser.length <= 0) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'User Khong ton tai',
+                    result: null
+                })
+            } else {
+                const result = await sequelize.query(`
                 DELETE FROM users WHERE id='${id}';
                     `, { type: QueryTypes.DELETE });
-                        resolve({ 
-                            errCode:0,
-                            errMessage: 'thành công',
-                            result:result
-                         })    
-                    }
-                          
-            } catch (error) {
-                 reject(error);
+                resolve({
+                    errCode: 0,
+                    errMessage: 'thành công',
+                    result: result
+                })
             }
-                 
-         }) 
-    }
-    
-    let getUserbyId = (id) => {
-        return new Promise(async(resolve, reject)=>{
-            try {
-                const result = await sequelize.query(`
+
+        } catch (error) {
+            reject(error);
+        }
+
+    })
+}
+
+let getUserbyId = (id) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const result = await sequelize.query(`
                 SELECT *
                 FROM users
                  WHERE id='${id}';
                     `, { type: QueryTypes.SELECT });
-                  
-                    if(result.length<=0){
-                        resolve({ 
-                            errCode:1,
-                            errMessage: 'User Khong ton tai',
-                            result:null
-                         })     
-                    }else{
-                        resolve({ 
-                            errCode:0,
-                            errMessage: 'thành công',
-                            result:result
-                         })    
-                    }
-                          
-            } catch (error) {
-                 reject(error);
+
+            if (result.length <= 0) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'User Khong ton tai',
+                    result: null
+                })
+            } else {
+                resolve({
+                    errCode: 0,
+                    errMessage: 'thành công',
+                    result: result
+                })
             }
-                 
-         })
-    }
-    const updateUserInfo = (data) => {
-        return new Promise(async(resolve, reject)=>{
-            try {
-                const result = await sequelize.query(`
+
+        } catch (error) {
+            reject(error);
+        }
+
+    })
+}
+const updateUserInfo = (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const result = await sequelize.query(`
                 UPDATE users
                 SET firstName = '${data.firstname}',lastName = '${data.lastname}',email = '${data.email}',password = '${data.password}',phoneNumber = '${data.phonenumber}',address = '${data.address}'
                 WHERE id='${data.id}';
                     `, { type: QueryTypes.UPDATE });
-              
-                    resolve({ 
-                        errCode:0,
-                        errMessage: 'thành công',
-                        result:result
-                     })    
-                     
-            } catch (error) {
-                 reject(error);
-            }
-                 
-         }) 
 
+            resolve({
+                errCode: 0,
+                errMessage: 'thành công',
+                result: result
+            })
 
+        } catch (error) {
+            reject(error);
+        }
 
-        
-    }
+    })
 
-module.exports  = {
-    handleGetUser:handleGetUser,
-    handleCreateUser:handleCreateUser,
-    deleteUser:deleteUser,
-    getUserbyId:getUserbyId,
-    updateUserInfo:updateUserInfo,
+}
+
+module.exports = {
+    handleGetUser: handleGetUser,
+    handleCreateUser: handleCreateUser,
+    deleteUser: deleteUser,
+    getUserbyId: getUserbyId,
+    updateUserInfo: updateUserInfo,
 }
