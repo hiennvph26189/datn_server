918edf7848c65ff9b0b808a6e5ffc9650e2b0e9a
saonguyen <jorrynguyen@gmail.com>
2023-09-12 18:12:57 +0700

    update

--- src/controllers/WebBanHangController/menuController.js
+++ src/controllers/WebBanHangController/menuController.js
@@ -1,4 +1,15 @@
 import menuService from "../../services/webbanhangService/menuService";
+let getConvertArrProduct = (arrData)=>{
+    const newArray = arrData.map(item => {
+      return {
+        ...item,
+        giaSanPham: item.giaSanPham.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' }),
+        giaBanSale: (item.giaSanPham-(item.giaSanPham*(item.sale/100))).toLocaleString('vi-VN', { style: 'currency', currency: 'VND' }),
+        image: JSON.parse(item.image)[0] // Chỉ lấy phần tử đầu tiên của mảng img
+      };
+    }); 
+    return newArray
+  }
 let getCategoryMenu = async (req, res) => {
   
     try {
@@ -18,9 +29,14 @@ let getCategoryProducts = async (req, res) => {
   
     try {
         let id = req.query.id
-        // let data = await menuService.getCategoriesService();
-       
-        return res.render("webBanHang/danhSachSanPham.ejs",{id:id})
+        let page = req.query.page
+     
+        let getCategoryProducts = await menuService.getDanhSachSanPhamIdService(id,page)
+        let nameCategories = getCategoryProducts.nameCategories
+        let totalCountProducts = getCategoryProducts.totalCount
+        let arrProducts = getConvertArrProduct(getCategoryProducts.products)
+        console.log(page)
+        return res.render("webBanHang/danhSachSanPham.ejs",{getCategoryProducts:arrProducts,nameCategories:nameCategories,totalCountProducts:totalCountProducts,id:id,page:parseInt(page)})
      } catch (error) {
          console.log("Lỗi phân quyền",error)
         return res.status(200).json({
--- src/services/webbanhangService/menuService.js
+++ src/services/webbanhangService/menuService.js
@@ -25,7 +25,47 @@ let getCategoriesService = ()=>{
          
      }) 
 }
+let getDanhSachSanPhamIdService = (id,page)=>{
+    return new Promise(async(resolve, reject)=>{
+       
+        try {
+            let nameCategories =  await sequelize.query(`
+            SELECT name FROM  categories where id = ${id} 
+                `, { type: QueryTypes.SELECT });
+           
+            let totalCount = await sequelize.query(`
+            SELECT COUNT(*) as total FROM  products where idDanhSach = ${id} 
+                `, { type: QueryTypes.SELECT });
+            let pageNumber = page;
+            let limit = 6; // Số lượng sản phẩm trên mỗi trang
+            let offset = (pageNumber - 1) * limit;
+            let  products = await sequelize.query(`
+            SELECT id,tenSp,giaSanPham,sale,image,luotMua FROM  products where idDanhSach = ${id} order by id desc limit ${limit} OFFSET ${offset}
+                `, { type: QueryTypes.SELECT });
+            
+            let totalPages = Math.ceil(totalCount[0].total / limit);
+           
+            if(products.length > 0 && nameCategories.length >0){
+                resolve({ 
+                    errCode:0,
+                    errMessage: 'thành công',
+                    products:products,
+                    nameCategories: nameCategories[0].name,
+                    totalCount:totalPages
+                 })
+            }
+                
+                      
+  
+        } catch (error) {
+             reject(error);
+        }
+         
+         
+     }) 
+}
 
 module.exports  = {
     getCategoriesService:getCategoriesService,   
+    getDanhSachSanPhamIdService:getDanhSachSanPhamIdService
 }
