b216f7b2bc41de25f01e3619eb793d4cfa2a9cf2
Nguyendz102 <thenguyen9603@gmail.com>
2023-11-27 19:11:07 +0700

    all

--- src/services/webQuanTriService/contactServieceADMIN.js
+++ src/services/webQuanTriService/contactServieceADMIN.js
@@ -48,10 +48,46 @@ let handlePutPhanHoiContactSeviceADMIN = (data)=>{
      }) 
 }
 
+let getUserWithPagination = (page)=>{
+    return new Promise(async(resolve, reject)=>{
+        try {
+            let totalCount = await sequelize.query(`
+                    SELECT COUNT(*) as total FROM  contact 
+                        `, { type: QueryTypes.SELECT });
+                    let pageNumber = page;
+                   
+                    let limit = 10; // Số lượng sản phẩm trên mỗi trang
+                    let offset = (pageNumber - 1) * limit;
+                    let  lienhe = await sequelize.query(`
+                    SELECT * FROM  contact  order by id desc limit ${limit} OFFSET ${offset}
+                        `, { type: QueryTypes.SELECT });
+                    
+                    let totalPages = Math.ceil(totalCount[0].total / limit);
+                   
+                    if(lienhe.length > 0 ){
+                        resolve({ 
+                            errCode:0,
+                            errMessage: 'thành công',
+                            lienhe:lienhe,
+                            nameCategories: "Sản phẩm bán chạy nhất",
+                            totalCount:totalPages
+                        })
+                    }    
+  
+        } catch (error) {
+             reject(error);
+        }
+     }) 
+}
+
+
+
+
 
 
 
 module.exports  = {
     handleGetContact:handleGetContact,
     handlePutPhanHoiContactSeviceADMIN:handlePutPhanHoiContactSeviceADMIN,
+    getUserWithPagination:getUserWithPagination,
 }
